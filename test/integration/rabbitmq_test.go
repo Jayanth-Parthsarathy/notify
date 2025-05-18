be6a536c46cd48ee2c6802e7b908a011a9446530
Jayanth-Parthsarathy <jparthaa@gmail.com>
2025-05-18 19:52:29 +0530

    refactor: add basic integration tests for utils

---
+++ test/integration/rabbitmq_test.go
@@ -0,0 +1,39 @@
+package integration
+
+import (
+	"os"
+	"testing"
+
+	"github.com/jayanth-parthsarathy/notify/internal/common/util"
+)
+
+
+func TestRabbitMQConnection(t *testing.T) {
+	os.Setenv("RABBIT_MQ_URL", "amqp://guest:guest@localhost:5672/")
+
+	conn, ch := util.ConnectToRabbitMQ()
+	defer conn.Close()
+	defer ch.Close()
+
+	if conn.IsClosed() {
+		t.Fatal("Expected connection to be open")
+	}
+
+	if ch == nil {
+		t.Fatal("Expected channel to be non-nil")
+	}
+}
+
+func TestRabbitMQDeclareQueue(t *testing.T) {
+	os.Setenv("RABBIT_MQ_URL", "amqp://guest:guest@localhost:5672/")
+
+	conn, ch := util.ConnectToRabbitMQ()
+	defer conn.Close()
+	defer ch.Close()
+
+	q := util.DeclareQueue(ch)
+
+	if q == nil || q.Name != "notification_queue" {
+		t.Fatalf("Expected queue 'notification_queue', got: %v", q)
+	}
+}
